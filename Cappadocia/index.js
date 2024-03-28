a650bd0aba3b18d37fad2b6debea2fdc1b86bf2a
Tolobek <tsharsheev002@gmail.com>
2024-03-28 14:37:54 +0600

    first commit

---
+++ Cappadocia/index.js
@@ -0,0 +1,17 @@
+let age = Number (prompt("how old are you?"))
+
+if (age <=5) {
+    alert("hi baby");
+}
+else if(age <=18) {
+    alert("salam okuuchu");
+}
+else if(age <=25) {
+    alert("hi student");
+}
+else if(age <=50) {
+    alert("salamatsyzby");
+}
+else if(age <=85) {
+    alert("hi dad");
+}
